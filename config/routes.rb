1ee97418382fb540a39311e090e543839b4146e4
tmarkssf <tmarkssf@gmail.com>
2016-05-13 07:02:12 +0000

    Added NavBar and Conact page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get '/about' => 'pages#about'
-
+  get '/contact' => 'pages#contact'
 
   root 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
